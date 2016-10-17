b7d69f6e25a0d44445053cfa174e3c32a57c51b1
Shameel Abdullah <shameel.abdullah@gmail.com>
2016-10-17 19:57:58 -0400

    switching to twitch tab now toggles mute

--- background.js
+++ background.js
@@ -2,7 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-chrome.tabs.onActivated.addListener(function(obj) {	
-	console.log(obj);
-	chrome.tabs.sendMessage(obj.tabId, {test:"TEST"},function(response){});
+
+chrome.tabs.onActivated.addListener(function(obj) {
+    // console.log(obj);
+    // chrome.tabs.sendMessage(obj.tabId, {test:"TEST"},function(response){});
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { greeting: "hello" }, function(response) {
+            console.log("test");
+        });
+    });
 });
