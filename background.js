f16324acc11ab8b69a6876b0e10469e1ffa4d7a2
Shameel Abdullah <shameel.abdullah@gmail.com>
2016-10-17 20:32:02 -0400

    trying to get old tab to mute

--- background.js
+++ background.js
@@ -2,13 +2,34 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+//for doing initial mute
+// chrome.browserAction.onClicked.addListener(function(tab) {
+//     chrome.tabs.query({ currentWindow: true, url: "https://www.twitch.tv/*" }, function(tabs) {
+//         for (var i = 0; i < tabs.length; i++) {
+//             chrome.tabs.executeScript(tabs[i].id, { file: "intialMute.js" });
+//         }
+//     })
+// });
+
+
+var oldTabId = 0;
 
 chrome.tabs.onActivated.addListener(function(obj) {
     // console.log(obj);
     // chrome.tabs.sendMessage(obj.tabId, {test:"TEST"},function(response){});
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    	if (oldTabId == 0){
+    		oldTabId = tabs[0].id;
+    		return;
+    	}    	
+
         chrome.tabs.sendMessage(tabs[0].id, { greeting: "hello" }, function(response) {
-            console.log("test");
         });
+
+        // chrome.tabs.sendMessage(oldTabId, { greeting: "hello" }, function(response) {
+        // });
+  
+        oldTabId = tabs[0].id;
+
     });
 });
--- content_script.js
+++ content_script.js
@@ -1,5 +1,6 @@
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
+  	console.log("client");
     $(".player-button--volume").click();
   });
