06f54983ecdeebf4b0e6cc74306809b9da4721db
Shameel Abdullah <shameel.abdullah@gmail.com>
2016-10-19 12:59:43 -0400

    initial mute now works

--- intialMute.js
+++ intialMute.js
@@ -1,2 +1,7 @@
-var test = document.getElementsByClassName("player");
-console.log(test.dataset.muted);
+var player = document.getElementsByClassName("player");
+
+if (player[0].dataset.muted != "true") {	
+	//not muted
+	var muteSwitch = document.getElementsByClassName("player-button--volume");
+	muteSwitch[0].click();	
+}
